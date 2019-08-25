a10a4ffc003f44b7379832bdbc2103b001f76de0
Matt Briggs <brigmatt>
2019-08-24 21:21:48 -0700

    exercise 1.8

---
+++ Exercise-001.006.rkt
@@ -0,0 +1,70 @@
+#lang racket
+
+; Prerequisite Definitions
+
+(define (abs x) ((if (< x 0) - +) x))
+
+"test (abs -1)"
+(abs -1)
+
+(define (square x) (* x x))
+
+"test (square -2)"
+(square -2)
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x )) 0.001))
+
+"test (good-enough? 3 9)"
+(good-enough? 3 9)
+
+"test (good-enough? 2 9)"
+(good-enough? 2 9)
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+"test (average 3.0 4.0)"
+(average 3.0 4.0)
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+"test (improve 1.9 4)"
+(improve 1.8 4)
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+"test (sqrt-iter 1.0 81.0)"
+(sqrt-iter 1.0 81.0)
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+"test (sqrt 9.0)"
+(sqrt 9.0)
+
+; Exercise 1.6: Alyssa P. Hacker doesn’t see why if needs to
+; be provided as a special form. “Why can’t I just define it
+; as an ordinary procedure in terms of cond?” she asks.
+; Alyssa’s friend Eva Lu Ator claims this can indeed be done,
+; and she defines a new version of if:
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+"test (new-if (> 1 0) 1 0 )"
+(new-if (> 1 0) 1 0 )
+"test (new-if (< 1 0) 1 0 )"
+(new-if (< 1 0) 1 0 )
+
+; Delighted, Alyssa uses new-if to rewrite the square-root
+; program:
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)

8e7f9d826bcbd65f547a41438ebf1a7dda07500d
Matt Briggs <brigmatt>
2019-08-27 09:03:55 -0700

    exercise 1.9 wip

---
+++ Exercise-1.10.rkt
@@ -0,0 +1,54 @@
+#lang racket
+"Excersize 1.10"
+
+; Exercise 1.10: e following procedure computes a math- ematical function called Ackermann’s
+; function.
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+; What are the values of the following expressions?
+
+"(A 1 10)"
+(A 1 10)
+"(A 2 4)"
+(A 2 4)
+"(A 3 3)"
+(A 3 3)
+"(A 1 1)"
+(A 1 1)
+"(A 1 2)"
+(A 1 2)
+"(A 1 3)"
+(A 1 3)
+"(A 1 1)"
+(A 1 1)
+"(A 2 1)"
+(A 2 1)
+"(A 3 1)"
+(A 3 1)
+
+; Expansion pattern for (A 1 10)
+(A 1 10)
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024

1175758161a49eb6272c5934bbcf6d6408c883d7
Matt Briggs <brigmatt>
2019-08-29 08:44:40 -0700

    exercise 1.14 - wip

---
+++ Exercise-1.14.rkt
@@ -0,0 +1,17 @@
+#lang racket
+"Excersize 1.14"
+
+; Exercise 1.14: 
+
+
+(define (count-change amount) (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination
+                         kinds-of-coins))
+                     kinds-of-coins)))))

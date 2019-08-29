d24676e524509670b3434144de5711ab15751573
Matt Briggs <brigmatt>
2019-08-28 20:08:39 -0700

    exercise 1.11 - failed

--- Exercise-1.11.rkt
+++ Exercise-1.11.rkt
@@ -6,39 +6,48 @@
 ; f(n)=f(n−1)+2f(n−2)+3f(n−3) if n≥3.
 
 ; Write a procedure that computes f by means of a recursive process.
-
-(define (f n) 
+(define (f n)
   (if (< n 3)
       n
       (+ (f (- n 1))
-         (* 2 (f (- n 2))
-         (* 3 (f (- n 3)))))))
-
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+         
+"(f 2)"
+(f 2)
 "(f 3)"
 (f 3)
 "(f 4)"
 (f 4)
 "(f 5)"
 (f 5)
+"(f 6)"
+(f 7)
 
 ; Write a procedure that computes f by means of an iterative process.
 
-(define (the-func a b c)
-  (+ a (* 2 b) (* 3 c)))
-
-(define (g-iter a b c)
-  (if (< a 3)
-      a
-      (g-iter (the-func a b c)
-              a
-              b)))
+(define (g-iter n-1 n-2 n-3 current stop)
+  (if (= current stop)
+      n-1
+      (g-iter (+ n-1 (* 2 n-2) (* 3 n-3)) n-1 n-2 (+ 1 current) stop)))
 
 (define (g n)
-  (g-iter n (- n 1) (- n 2)))
+  (g-iter 2 1 0 2 n))
 
+"(g 2)"
+(g 2)
 "(g 3)"
 (g 3)
 "(g 4)"
 (g 4)
 "(g 5)"
 (g 5)
+"(g 6)"
+(g 7)
+
+; Note: ouch, I used these:
+; https://codology.net/post/sicp-solution-exercise-1-11/
+; http://jots-jottings.blogspot.com/2011/08/sicp-111-recursive-and-iterative.html
+;
+; Could not solve on my own
+; TODO - try again.

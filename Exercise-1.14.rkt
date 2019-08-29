91e399e24f067b2d1df978253e6b0c0c74b1c1c1
Matt Briggs <brigmatt>
2019-08-29 09:01:09 -0700

    exercise 1.14 - wip

--- Exercise-1.14.rkt
+++ Exercise-1.14.rkt
@@ -4,6 +4,8 @@
 ; Exercise 1.14: 
 
 
+; this function takes the amount to make change for as 'amount'. The number of types of coins is
+; hardcoded at 5, and those denominations are hardcoded as 1, 5, 10, 25 and 50.
 (define (count-change amount) (cc amount 5))
 
 (define (cc amount kinds-of-coins)
@@ -16,6 +18,9 @@
                          kinds-of-coins))
                      kinds-of-coins)))))
 
+; takes the number of coin denominations, and returns the largest valued coin for that set of
+; denominations. for example, if we have pennies, nickles and dimes, this function takes 3
+; and returns the value of a dime.
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)

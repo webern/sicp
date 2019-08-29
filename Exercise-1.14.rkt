bb9fd271150202a7e342b81b5a6be237c876b14c
Matt Briggs <brigmatt>
2019-08-29 08:48:01 -0700

    exercise 1.14 - wip

--- Exercise-1.14.rkt
+++ Exercise-1.14.rkt
@@ -15,3 +15,10 @@
                         (first-denomination
                          kinds-of-coins))
                      kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))

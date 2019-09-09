7bd99d70071c8b701791ac7e7922d15e498f925c
Matt Briggs <brigmatt>
2019-09-08 19:55:55 -0700

    paste in exercises 1.17 an 1.18

---
+++ Exercise-1.17.rkt
@@ -0,0 +1,18 @@
+#lang racket
+"Excersize 1.17"
+
+; Exercise 1.17: The exponentiation algorithms in this sec- tion are based on performing
+; exponentiation by means of repeated multiplication. In a similar way, one can perform integer
+; multiplication by means of repeated addition. The following multiplication procedure (in which it
+; is assumed that our language can only add, not multiply) is analogous to the expt procedure:
+
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+; This algorithm takes a number of steps that is linear in b. Now suppose we include, together with 
+; addition, opera- tions double, which doubles an integer, and halve, which divides an (even)
+; integer by 2. Using these, design a mul- tiplication procedure analogous to fast-expt that uses a
+; logarithmic number of steps.
+

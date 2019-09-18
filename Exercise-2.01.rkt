a01edead672a9bb82ec4916de35b8bbbab6b35eb
Matt Briggs <brigmatt>
2019-09-18 08:39:28 -0700

    exercise 2.01 wip

--- Exercise-2.01.rkt
+++ Exercise-2.01.rkt
@@ -14,47 +14,31 @@
 (define (numer rat) (car rat))
 (define (denom rat) (cdr rat))
 
-(make-rat-old 1 2)
+; corrects signs and does gcd
+(define (make-rat n d)
+  (define (flip-numer n d) ; change numerator if necessary due to sign
+    (cond 
+      ((and (< n 0) (< d 0)) (* -1 n))
+      ((and (< n 0) (> d 0)) n)
+      ((and (> n 0) (< d 0)) (* -1 n))
+      (else n)))
+  (define (flip-denom d) ; change denominator if necessary due to sign
+    (if (< d 0)
+        (* -1 d)
+        d))
+  (let ((g (gcd n d))
+        (numer (flip-numer n d))
+        (denom (flip-denom d)))
+    (cons (/ numer g) (/ denom g))))
 
-(let ((r (make-rat-old 5 10)))
-  (numer r))
+"(make-rat -2 -2) should print (1 . 1)"
+(make-rat -2 -2)
 
-(let ((r (make-rat-old 1 2)))
-  (denom r))
+"(make-rat -21 7) should print (-3 . 1)"
+(make-rat -21 7)
 
-; (define (simple-and n d)
-;   (and (< n 0) (< d 0)))
+"(make-rat 13 -26) should print (-1 . 2)"
+(make-rat 13 -26)
 
-; (and thing1 thing2)
-
-; ()
-
-; (or (< n 0) (< d 0))
-
-; takes a numerator and denominator and returns the correct numerator adjusting for sign
-(define (flip-numer n d)
-  (cond 
-    ((and (< n 0) (< d 0)) (* -1 n))
-    ((and (< n 0) (> d 0)) n)
-    ((and (> n 0) (< d 0)) (* -1 n))
-    (else n))
-)
-
-; takes a numerator and denominator and returns the correct denominator adjusting for sign
-(define (flip-denom n d)
-  (if (< d 0)
-      (* -1 d)
-      d))
-
-(flip-denom -1 -2)
-(flip-denom 1 2)
-(flip-denom -1 2)
-(flip-denom 1 -2)
-
-; (define (make-rat-new n d)
-;   (let ((numer (cond 
-;                  (((and (< n 0) (< d 0)) n))
-;                  (((or (< n 0) (< d 0) (* -1 n))))
-;                  (else n)))
-;        (g (gcd n d))))
-;     (cons (/ n g) (/ d g)))
+"(make-rat 100 1000) should print (1 . 10)"
+(make-rat 100 1000)

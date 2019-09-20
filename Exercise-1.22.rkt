c4610d3ca1c99760901faccdadda4c9aee35eeef
Matt Briggs <brigmatt>
2019-09-20 09:34:57 -0700

    exercise 1.22 wip

--- Exercise-1.22.rkt
+++ Exercise-1.22.rkt
@@ -16,9 +16,9 @@
           ((divides? current n) current)
           (else (find-divisor n (+ current 1)))))
 
-(define (divides? a b) (= remainder b a) 0)
+(define (divides? a b) (= (remainder b a) 0))
 
-(define (prime? n) (= n 0))
+(define (prime? n) (= n (smallest-divisor n)))
 
 (define (runtime) (current-milliseconds))
 
@@ -30,9 +30,27 @@
 (define (start-prime-test n start-time)
     (if (prime? n)
         (report-prime (- (runtime) start-time))
-        (display " not prime ")))
+        (null))) ; ok? an 'else' block was required, need a no-op
 
 (define (report-prime elapsed-time)
     (display " *** ")
     (display elapsed-time))
 
+
+"test (prime? 10) -> expect false"
+(prime? 10)
+"test (prime? 11) -> expect true"
+(prime? 11)
+
+(define (even? n) (= (remainder n 2) 0))
+
+(define (search-for-primes start end prime-count)
+  (define (search-iter current-n primes-found)
+    (if (>= primes-found prime-count)
+        (prime-count)
+        (search-iter (+ current-n 2) (if (prime? current-n)
+                                         (+ prime-count 1)
+                                         (prime-count)))))
+  (search-iter (if (even? start) (+ start 1) start) 0))
+
+(search-for-primes 1 100 5)
